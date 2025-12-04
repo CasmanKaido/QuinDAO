ba1becdc33347f3821035f970dac3466d6627372
casman <cassidyladden1@gmail.com>
2025-12-04 17:07:43 +0100

    Simplify Timelock constructor to avoid array parameter issues

--- contracts/QuinDAOTimelock.sol
+++ contracts/QuinDAOTimelock.sol
@@ -6,12 +6,16 @@ import "@openzeppelin/contracts@4.9.3/governance/TimelockController.sol";
 /**
  * @title QuinDAOTimelock
  * @dev Timelock controller for QuinDAO governance
+ * Simplified deployment - roles can be granted after deployment
  */
 contract QuinDAOTimelock is TimelockController {
     constructor(
         uint256 minDelay,
-        address[] memory proposers,
-        address[] memory executors,
         address admin
-    ) TimelockController(minDelay, proposers, executors, admin) {}
+    ) TimelockController(
+        minDelay,
+        new address[](0),  // Empty proposers array - grant role after deployment
+        new address[](0),  // Empty executors array - grant role after deployment  
+        admin
+    ) {}
 }
